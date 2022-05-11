47123ab74aa400c55a7536daddd15fe392b6fd26
Serhii B <s_blyshchak@ukr.net>
2022-05-11 21:08:00 +0300

    Implement listens  logic

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 import { ObjectId } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
@@ -8,8 +18,15 @@ import { TrackService } from './track.service';
 export class TrackController {
   constructor(private trackService: TrackService) {}
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const { picture, audio } = files;
+    return this.trackService.create(dto, picture[0], audio[0]);
   }
   @Get()
   getAll() {
@@ -26,6 +43,11 @@ export class TrackController {
 
   @Post('/comment')
   addComment(@Body() dto: CreateCommentDto) {
-return this.trackService.addComment(dto)
+    return this.trackService.addComment(dto);
+  }
+
+  @Post('/listen/:id')
+  listen(@Param('id') id: ObjectId){
+    return this.trackService.listen(id)
   }
 }
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
+import { FileService, FileType } from 'src/file/file.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { Comment, CommentDocument } from './schemas/comment.schema';
@@ -11,9 +12,17 @@ export class TrackService {
   constructor(
     @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
     @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    private fileService: FileService,
   ) {}
-  async create(dto: CreateTrackDto): Promise<Track> {
-    const track = await this.trackModel.create({ ...dto, listens: 0 });
+  async create(dto: CreateTrackDto, picture, audio): Promise<Track> {
+    const audioPath = this.fileService.createFile(FileType.AUDIO, audio);
+    const picturePath = this.fileService.createFile(FileType.IMAGE, picture);
+    const track = await this.trackModel.create({
+      ...dto,
+      listens: 0,
+      audio: audioPath,
+      picture: picturePath,
+    });
     return track;
   }
   async getAll(): Promise<Track[]> {
@@ -21,20 +30,28 @@ export class TrackService {
     return tracks;
   }
   async getOne(id: ObjectId): Promise<Track> {
-    const track = await (await this.trackModel.findById(id)).populate('comments')
+    const track = await (
+      await this.trackModel.findById(id)
+    ).populate('comments');
     return track;
   }
   async delete(id: ObjectId): Promise<ObjectId> {
-    const track = await this.trackModel.findByIdAndDelete(id)
-    return track._id
+    const track = await this.trackModel.findByIdAndDelete(id);
+    return track._id;
   }
 
   async addComment(dto: CreateCommentDto): Promise<Comment> {
-const  track = await this.trackModel.findById(dto.trackId)
-const comment = await this.commentModel.create({...dto})
-track.comments.push(comment._id)
-await track.save()
-return comment
+    const track = await this.trackModel.findById(dto.trackId);
+    const comment = await this.commentModel.create({ ...dto });
+    track.comments.push(comment._id);
+    await track.save();
+    return comment;
+  }
 
+  async listen(id: ObjectId) {
+    const track = await this.trackModel.findById(id);
+    track.listens += 1;
+
+    track.save();
   }
 }
